b12cfa625c6c7584b98f6782972b46601739c143
vicchuu <vishnumaniplt@gmail.com>
2022-07-31 13:28:42 +0530

    change in req.txt

--- pybot.py
+++ pybot.py
@@ -1,110 +1,110 @@
-import nltk
-import numpy as np
-import torch
-import torch.nn as nn
-from itertools import groupby
-import ssl
-
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
+# import nltk
+# import numpy as np
+# import torch
+# import torch.nn as nn
+# from itertools import groupby
+# import ssl
 #
-# nltk.download()
-from nltk.stem.porter import PorterStemmer
-
-stemmer=PorterStemmer()
-def tokenise(sentence):
-    return nltk.word_tokenize(sentence)
-
-def stem(word):
-    return stemmer.stem(word.lower())
-
-def bagofwords(word,bagofwords):
-    bag=np.zeros(len(bagofwords),dtype=np.float32)
-    tokenizesentence=[stem(w) for w in word]
-
-    for word in  (tokenizesentence):
-        #print(word )
-        if word in bagofwords:
-            #print(word)
-            indx=bagofwords.index(word)
-            #print("index ",indx)
-            bag[indx]=1
-    return bag
-# pdf=[23,24,25,2,3]
-# lis1 =[5,4,3,4,2,1]  #list
-# lis2=(5,4,3,4,2,1)  #tuple
-# lis3={5,4,3,4,2,1}  # Set
-# lis4 = {9,8,7,6,5,4,3,2}
+# # try:
+# #     _create_unverified_https_context = ssl._create_unverified_context
+# # except AttributeError:
+# #     pass
+# # else:
+# #     ssl._create_default_https_context = _create_unverified_https_context
+# #
+# # nltk.download()
+# from nltk.stem.porter import PorterStemmer
 #
-# lisy = lambda x : x<10
+# stemmer=PorterStemmer()
+# def tokenise(sentence):
+#     return nltk.word_tokenize(sentence)
 #
-# for a in pdf:
-#     print(lisy(a))
+# def stem(word):
+#     return stemmer.stem(word.lower())
 #
-# newin = groupby(lis3, key=lambda x: x < 3)
+# def bagofwords(word,bagofwords):
+#     bag=np.zeros(len(bagofwords),dtype=np.float32)
+#     tokenizesentence=[stem(w) for w in word]
 #
-# for key,value  in newin:
-#     print(key,set(value))
+#     for word in  (tokenizesentence):
+#         #print(word )
+#         if word in bagofwords:
+#             #print(word)
+#             indx=bagofwords.index(word)
+#             #print("index ",indx)
+#             bag[indx]=1
+#     return bag
+# # pdf=[23,24,25,2,3]
+# # lis1 =[5,4,3,4,2,1]  #list
+# # lis2=(5,4,3,4,2,1)  #tuple
+# # lis3={5,4,3,4,2,1}  # Set
+# # lis4 = {9,8,7,6,5,4,3,2}
+# #
+# # lisy = lambda x : x<10
+# #
+# # for a in pdf:
+# #     print(lisy(a))
+# #
+# # newin = groupby(lis3, key=lambda x: x < 3)
+# #
+# # for key,value  in newin:
+# #     print(key,set(value))
+# #
+# # from functools import reduce
+# # plm = [1,2,3,4,5]
+# #
+# # print(reduce(lambda x,y : x<4,plm))
+# #
+# #
+# # class valuelow(Exception):
+# #     def __init__(self,message,value):
+# #         self.message=message
+# #         self.value=value
+# #
+# # def raiseError(x):
+# #     if x<10:
+# #         raise valuelow("too low buddy ",x)
+# #
+# # try:
+# #     raiseError(1)
+# # except valuelow as e:
+# #     print("looop" ,e.message, e.value)
+# #
+# #
+# # try:
+# #     a= 50
+# #     b = a + 10.0
+# # except TypeError as t:
+# #     print(t)
+# #
+# # #rint(lis4.update(lis3))
+# # print(lis2)
+# # print(lis3)
+# # print(lis4)
 #
-# from functools import reduce
-# plm = [1,2,3,4,5]
 #
-# print(reduce(lambda x,y : x<4,plm))
-#
-#
-# class valuelow(Exception):
-#     def __init__(self,message,value):
-#         self.message=message
-#         self.value=value
-#
-# def raiseError(x):
-#     if x<10:
-#         raise valuelow("too low buddy ",x)
-#
-# try:
-#     raiseError(1)
-# except valuelow as e:
-#     print("looop" ,e.message, e.value)
+# # def traceCall(func):
+# #
+# #     def wrapper():
+# #         print("inside")
+# #         func()
+# #         print("Outside")
+# #     return wrapper
+# # @traceCall
+# # def printout( ):
+# #     print("Vishnu")
+# #
+# # #obj = traceCall(printout("sweeet"),"swet")
+# #
+# # printout( )
 #
 #
-# try:
-#     a= 50
-#     b = a + 10.0
-# except TypeError as t:
-#     print(t)
 #
-# #rint(lis4.update(lis3))
-# print(lis2)
-# print(lis3)
-# print(lis4)
-
-
-# def traceCall(func):
+# word=["this" , "is", "hero","me  "]
+# bagOfWord=["my","name" ,"is","vishnu","do","you","know","me"]
+# d=bagofwords(word,bagOfWord)
+# print(d)
 #
-#     def wrapper():
-#         print("inside")
-#         func()
-#         print("Outside")
-#     return wrapper
-# @traceCall
-# def printout( ):
-#     print("Vishnu")
+# avail = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #
-# #obj = traceCall(printout("sweeet"),"swet")
-#
-# printout( )
-
-
-
-word=["this" , "is", "hero","me  "]
-bagOfWord=["my","name" ,"is","vishnu","do","you","know","me"]
-d=bagofwords(word,bagOfWord)
-print(d)
-
-avail = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-print(avail)
+# print(avail)
--- trainjson.py
+++ trainjson.py
@@ -1,49 +1,49 @@
-import json
-
-from pybot import tokenise,stem , bagofwords
-
-import numpy as np
-#import nltk.w
-from pybot import *
-
-with open("intent.json", 'r') as f:
-    intent=json.load(f)
-
-
-tags=[]
-patterns=[]
-allwords=[]
-xy=[]
-
-for content in intent["intents"]:
-    tag=content["tag"]
-    tags.append(tag)
-    for pattern in content["patterns"]:
-        pat= tokenise(pattern)
-        allwords.extend(pat)
-        xy.append((pattern,tag))
-
-print(len(allwords),allwords)
-"""Remove puncuation"""
-puncuations=[",",".","?","!","@","&","*","(",")"]
-allwords=[stem(w) for w in allwords if w not in puncuations ]
-allwords=sorted(set(allwords))
-tags=sorted(set(tags))
-print(len(allwords), allwords)
-
-xtrain=[]
-ytrain=[]
-
-for (pattern ,tag) in xy:
-
-    bag=bagofwords(pattern,allwords)
-    xtrain.append(bag)
-    label=tags.index(tag)
-    ytrain.append(label)
-
-xtrain= np.array(xtrain)
-ytrain=np.array(ytrain)
-
-
-
- 
+# import json
+#
+# from pybot import tokenise,stem , bagofwords
+#
+# import numpy as np
+# #import nltk.w
+# from pybot import *
+#
+# with open("intent.json", 'r') as f:
+#     intent=json.load(f)
+#
+#
+# tags=[]
+# patterns=[]
+# allwords=[]
+# xy=[]
+#
+# for content in intent["intents"]:
+#     tag=content["tag"]
+#     tags.append(tag)
+#     for pattern in content["patterns"]:
+#         pat= tokenise(pattern)
+#         allwords.extend(pat)
+#         xy.append((pattern,tag))
+#
+# print(len(allwords),allwords)
+# """Remove puncuation"""
+# puncuations=[",",".","?","!","@","&","*","(",")"]
+# allwords=[stem(w) for w in allwords if w not in puncuations ]
+# allwords=sorted(set(allwords))
+# tags=sorted(set(tags))
+# print(len(allwords), allwords)
+#
+# xtrain=[]
+# ytrain=[]
+#
+# for (pattern ,tag) in xy:
+#
+#     bag=bagofwords(pattern,allwords)
+#     xtrain.append(bag)
+#     label=tags.index(tag)
+#     ytrain.append(label)
+#
+# xtrain= np.array(xtrain)
+# ytrain=np.array(ytrain)
+#
+#
+#
+#
